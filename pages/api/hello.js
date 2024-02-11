8a71484c7e95c384eca15ac3f669a17d8ce1979e
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-02-11 17:45:28 +0530

    add endpoint in next

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// endpoints: http://localhost:3000/api/hello
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: "@codeguyakash!!" });
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,7 @@
 import { Inter } from "next/font/google";
-// import { useEffect, useState } from "react";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(props) {
-  // const [dataInfo, setDataInfo] = useState();
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const response = await fetch("https://api.codeguyakash.me/");
-
-  //     setDataInfo(await response.json());
-  //   }
-  //   fetchData();
-  // }, []);
-  // console.log(dataInfo);
   return (
     <>
       <main className={`items-center p-24 ${inter.className} h-[100vh]`}>
@@ -25,6 +13,9 @@ export default function Home(props) {
             <p className="text-center text-yellow-500 mt-6">
               {props.data && props.data.description}
             </p>
+            <p className="text-center text-green-500 mt-1">
+              {props.data2 && props.data2.name}
+            </p>
           </div>
         </div>
       </main>
@@ -34,9 +25,11 @@ export default function Home(props) {
 
 export const getServerSideProps = async () => {
   const data = await (await fetch("https://api.codeguyakash.me/")).json();
+  const data2 = await (await fetch("http://localhost:3000/api/hello/")).json();
   return {
     props: {
       data,
+      data2,
     },
   };
 };
